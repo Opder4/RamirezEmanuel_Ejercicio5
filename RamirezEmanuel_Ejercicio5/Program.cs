64bc39466a3087f41dd7319f90f41e328bced41e
Emanuel Ramírez <er6348100@gmail.com>
2026-02-21 22:05:04 -0600

    Implementación de menú y datos de entrada

--- RamirezEmanuel_Ejercicio5/Program.cs
+++ RamirezEmanuel_Ejercicio5/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("");
+﻿Console.WriteLine("Ingrese el tipo de solicitante: ");
+Console.WriteLine("");
+Console.WriteLine("1. Empleado fijo");
+Console.WriteLine("2. Temporal");
+Console.WriteLine("3. Independiente");
+Console.WriteLine("4. Estudiante");
+Console.WriteLine("");
+int solicitante=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese el salario mensual: ");
+Console.WriteLine("");
+double salario=double.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese la antigüedad laboral (meses): ");
+Console.WriteLine("");
+int meses=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese el monto solicitado: ");
+Console.WriteLine("");
+Console.WriteLine("Ingrese el Historial crediticio: ");
+Console.WriteLine("");
+Console.WriteLine("1. Excelente");
+Console.WriteLine("2. Bueno");
+Console.WriteLine("3. Regular");
+Console.WriteLine("4. Malo");
+Console.WriteLine("");
+int historialcrediticio=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+double montosolicitado=double.Parse(Console.ReadLine());
+Console.WriteLine("");
+bool fiador = false;
+if  (solicitante==1 || historialcrediticio==4)
+{
+    fiador = true;
+}
